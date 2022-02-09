355a84a3093e441fe8db3b1bdf77e0dd645c9731
yuwen159 <1341603277@qq.com>
2022-02-09 14:11:25 +0800

    Functional test now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,45 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+import unittest
+import time
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        
+        time.sleep(3)
+        self.assertIn('TO-DO', self.browser.title)
+        
+        header_text = self.browser.find_element(By.TAG_NAME,"h1").text
+        self.assertIn('TO-DO', header_text)
+
+        inputbox = self.browser.find_element(By.ID, "id_new_item")
+        self.assertEqual(
+                imputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+                )
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+
+        table = self.browser.find_element(By.ID,"id_list_table")
+        rows = table.find_elements(By.TAG_NAME,"tr")
+        self.assertTrue(
+                any(row.text == '1: Buy peacock feathers' for row in rows)
+                )
+
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
+
+
+
+
---
+++ lists/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home_page(request):
+    return HttpResponse('<html><title>TO-DO</title></html>')
+
+
